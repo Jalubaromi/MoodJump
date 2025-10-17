96d4e9b4cf38780db08492be360f176122713308
B <baptistesalazar@gmail.com>
2025-10-17 10:47:49 +0200

    Refacto: Archi

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const pool = require('./db'); // Importe le pool de connexion
+const pool = require('./config/db'); // Importe le pool de connexion
 const app = express();
 const port = 3001;
 
